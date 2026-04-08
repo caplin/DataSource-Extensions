cf51b4d8d4e09b71f4f68782c56f54f13b9ae237
Ross Anderson <ross.anderson@caplin.com>
2026-04-08 14:54:17 +0100

    Allow exception types to be preserved when serializing ValueOrCompletion objects

--- util/src/test/kotlin/com/caplin/integration/datasourcex/util/serialization/fory/ValueOrCompletionSerializationTest.kt
+++ util/src/test/kotlin/com/caplin/integration/datasourcex/util/serialization/fory/ValueOrCompletionSerializationTest.kt
@@ -3,8 +3,11 @@ package com.caplin.integration.datasourcex.util.serialization.fory
 import com.caplin.integration.datasourcex.util.flow.ValueOrCompletion
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 import org.apache.fory.Fory
 import org.apache.fory.config.Language
+
+class CustomException(val customMessage: String) : Exception(customMessage)
 
 class ValueOrCompletionSerializationTest :
     FunSpec({
@@ -12,6 +15,15 @@ class ValueOrCompletionSerializationTest :
           Fory.builder()
               .withLanguage(Language.JAVA)
               .requireClassRegistration(false)
+              .withRefTracking(true)
+              .build()
+              .registerDataSourceSerializers(preserveExceptionTypes = true)
+
+      val foryNoType =
+          Fory.builder()
+              .withLanguage(Language.JAVA)
+              .requireClassRegistration(false)
+              .withRefTracking(false)
               .build()
               .registerDataSourceSerializers()
 
@@ -37,11 +49,22 @@ class ValueOrCompletionSerializationTest :
           deserialized shouldBe event
         }
 
-        test("Completion") {
-          val event: ValueOrCompletion.Completion = ValueOrCompletion.Completion(null)
+        test("Completion with custom exception (preserved type)") {
+          val event: ValueOrCompletion.Completion =
+              ValueOrCompletion.Completion(CustomException("aah"))
           val bytes = fory.serialize(event)
-          val deserialized = fory.deserialize(bytes)
-          deserialized shouldBe event
+          val deserialized = fory.deserialize(bytes) as ValueOrCompletion.Completion
+          val throwable = deserialized.throwable.shouldBeInstanceOf<CustomException>()
+          throwable.customMessage shouldBe "aah"
+        }
+
+        test("Completion with exception (no type)") {
+          val event: ValueOrCompletion.Completion =
+              ValueOrCompletion.Completion(IllegalStateException("aah"))
+          val bytes = foryNoType.serialize(event)
+          val deserialized = foryNoType.deserialize(bytes) as ValueOrCompletion.Completion
+          deserialized.throwable.shouldBeInstanceOf<RuntimeException>()
+          deserialized.throwable?.message shouldBe "aah"
         }
       }
     })
